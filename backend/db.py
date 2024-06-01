0afe722552689f32bd578ef85a68929d76b35c02
JakubOleksy1 <jkoleksy1@gmail.com>
2024-06-01 17:07:44 +0200

    New changes

--- backend/db.py
+++ backend/db.py
@@ -2,15 +2,26 @@ from pymongo import MongoClient, ReturnDocument
 from bson.objectid import ObjectId
 import os
 
-mongodb_host = os.environ.get("MONGODB_HOST", "localhost")
-connection_string = f"mongodb://root:example@{mongodb_host}"
-client = MongoClient(connection_string)
-print(f"Connecting to DB on URL: {connection_string}")
-db = client["devops_db"]
-users = db.users
+mongodb_rw_host = os.environ.get("MONGODB_RW_HOST", "localhost")
+mongodb_ro_host = os.environ.get("MONGODB_RO_HOST", "localhost")
+
+rw_connection_string = f"mongodb://root:example@{mongodb_rw_host}"
+ro_connection_string = f"mongodb://root:example@{mongodb_ro_host}"
+
+rw_client = MongoClient(rw_connection_string)
+ro_client = MongoClient(ro_connection_string)
+
+print(f"Connecting to RW DB on URL: {rw_connection_string}")
+print(f"Connecting to RO DB on URL: {ro_connection_string}")
+
+rw_db = rw_client["devops_db"]
+ro_db = ro_client["devops_db"]
+
+users_rw = rw_db.users
+users_ro = ro_db.users
 
 def get_users():
-    users_cursor = users.find({})
+    users_cursor = users_ro.find({})
     users_list = []
     for user in users_cursor:
         user['_id'] = str(user['_id'])
@@ -18,25 +29,24 @@ def get_users():
         users_list.append(user)
     return users_list
 
-
 def create_user(user_data):
-    if users.count_documents({"$or": [{"email": user_data["email"]}]}):
+    if users_rw.count_documents({"$or": [{"email": user_data["email"]}]}):
         return "User with this email already exists."
 
-    return str(users.insert_one(user_data).inserted_id)
+    return str(users_rw.insert_one(user_data).inserted_id)
 
 def read_user(email=None, user_id=None):
     if email:
-        user = users.find_one({"email": email})
+        user = users_ro.find_one({"email": email})
     else:
-        user = users.find_one({"_id": ObjectId(user_id)})
+        user = users_ro.find_one({"_id": ObjectId(user_id)})
 
     if user:
         user['_id'] = str(user['_id'])
     return user
 
 def update_user(user_id, new_user_data):
-    user = users.find_one_and_update(
+    user = users_rw.find_one_and_update(
         {"_id": ObjectId(user_id)},
         {"$set": new_user_data},
         return_document=ReturnDocument.AFTER
@@ -46,7 +56,7 @@ def update_user(user_id, new_user_data):
     return user
 
 def delete_user(user_id):
-    return users.delete_one({"_id": ObjectId(user_id)})
+    return users_rw.delete_one({"_id": ObjectId(user_id)})
 
 if __name__ == "__main__":
 
